3abeb2d8ca47e12175d60dd0e3aff8faef323dc5
SurajrajendraJaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2022-03-26 13:58:02 +0530

    add Info header

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
+import FrontInfo from './FrontInfo';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
+        <FrontInfo />
         <CellList />
       </div>
     </Provider>
